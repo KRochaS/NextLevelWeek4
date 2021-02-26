a76194bcdcdce214d30a53d2fbdd11a18eb64d71
KRochaS <karinerocha.s@hotmail.com>
2021-02-26 20:41:56 -0300

    Profile

--- trilha-reactjs/src/components/Profile.tsx
+++ trilha-reactjs/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+
+    const { level } = useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/KRochaS.png" alt="Karine Rocha" />
@@ -9,8 +13,7 @@ export function Profile() {
                 <strong> Karine Rocha </strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
-                    Level 1
-
+                    Level {level}
                  </p>
             </div>
         </div>
