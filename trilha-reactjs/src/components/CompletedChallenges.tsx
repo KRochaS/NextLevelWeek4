a9ac25e8935cc14d7f09a7d2b85ba8680202a154
KRochaS <karinerocha.s@hotmail.com>
2021-03-01 22:41:16 -0300

    CompletedChallenges

--- trilha-reactjs/src/components/CompletedChallenges.tsx
+++ trilha-reactjs/src/components/CompletedChallenges.tsx
@@ -1,14 +1,19 @@
 import { useContext } from 'react';
 
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { ThemeContext } from '../contexts/ThemeContext';
 import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges() {
     const { challengesCompleted } = useContext(ChallengesContext);
+    const { isDarkMode } = useContext(ThemeContext);
     return (
         <div className={styles.completedChallengesContainer}>
+            <div className={isDarkMode ? styles.textDark : ''}>
             <span> Desafios Completos </span>
             <span>  {challengesCompleted} </span>
+
+            </div>
         </div>
     );
 }
