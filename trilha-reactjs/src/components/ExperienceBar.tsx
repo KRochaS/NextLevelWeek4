29cd154fe4e0bbb90a09c801b2d01798de13ac9f
KRochaS <karinerocha.s@hotmail.com>
2021-03-01 22:41:36 -0300

    ExperienceBar

--- trilha-reactjs/src/components/ExperienceBar.tsx
+++ trilha-reactjs/src/components/ExperienceBar.tsx
@@ -1,29 +1,51 @@
 import { useContext } from 'react';
 
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { ThemeContext } from '../contexts/ThemeContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
     const { currentExperience, experienceToNextLevel } = useContext(
-      ChallengesContext,
+        ChallengesContext,
     );
-  
+
+    const { isDarkMode, darkMode } = useContext(ThemeContext);
+
     const percentToNextLevel =
-      Math.round(currentExperience * 100) / experienceToNextLevel;
-  
+        Math.round(currentExperience * 100) / experienceToNextLevel;
+
+        function handleIsThemeDark() {
+
+            darkMode();
+    
+          
+        }
+
     return (
-      <header className={styles.experienceBar}>
-        <span>0 xp</span>
-        <div>
-          <div style={{ width: `${percentToNextLevel}%` }} />
-          <span
-            className={styles.currentExperience}
-            style={{ left: `${percentToNextLevel}%` }}
-          >
-            {currentExperience} xp
+
+        <header className={styles.experienceBar}>
+            <span className={isDarkMode ? styles.textDark : ''}>0 xp</span>
+            <div>
+                <div style={{ width: `${percentToNextLevel}%` }} />
+                <span
+                    className={!isDarkMode ? styles.currentExperience : styles.currentExperienceDark}
+                    style={{ left: `${percentToNextLevel}%` }}
+                >
+                    {currentExperience} xp
           </span>
-        </div>
-        <span>{experienceToNextLevel} xp</span>
-      </header>
+            </div>
+            <span className={isDarkMode ? styles.textDark : ''} >{experienceToNextLevel} xp</span>
+
+           
+            <button type="button" className={styles.darkMode} onClick={handleIsThemeDark}>
+                <img src={!isDarkMode ? `icons/moon.svg` : `icons/sun.svg`} alt="level" />
+
+            </button>
+
+        </header>
+
+
+
+
     );
-  }
+}
