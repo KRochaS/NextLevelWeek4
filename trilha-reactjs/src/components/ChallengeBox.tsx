9f7dabea88ed99c1044e590dcc21e83ba50faaa9
KRochaS <karinerocha.s@hotmail.com>
2021-03-01 22:40:52 -0300

    ChallengeBox

--- trilha-reactjs/src/components/ChallengeBox.tsx
+++ trilha-reactjs/src/components/ChallengeBox.tsx
@@ -24,51 +24,57 @@ export function ChallengeBox() {
         resetCountdown();
     }
 
-  
+
     return (
-        
+
         <div className={!isDarkMode ? styles.challengeBoxContainer : styles.challengeBoxContainerDark}>
 
-                {
-                    activeChallenge ? (
-						<div className={styles.challengeActive}> 
-							<header> Ganhe {activeChallenge.amount} xp</header>
-						
+            {
+                activeChallenge ? (
+                    <div className={styles.challengeActive}>
+                        <header> Ganhe {activeChallenge.amount} xp</header>
 
-						<main>
-							<img src={`icons/${activeChallenge.type}.svg`} alt=""/>
-							<strong> Novo desafio</strong>
-							<p>  { activeChallenge.description} </p>
-						</main>
 
-						<footer>
-							<button
-								type="button"
-								className={styles.challengeFailedButton}
+                        <main>
+                            <img src={`icons/${activeChallenge.type}.svg`} alt="" />
+                            <div className={isDarkMode ? styles.challengeActiveTextDark : ''} >
+                                <strong> Novo desafio</strong>
+                                <p>  {activeChallenge.description} </p>
+
+                            </div>
+                        </main>
+
+                        <footer>
+                            <button
+                                type="button"
+                                className={styles.challengeFailedButton}
                                 onClick={handleChallengeFailed}
-							
-							> Falhei</button>
-							<button
-									type="button"
-									className={styles.challengeSucceededButton}
-                                    onClick={handleChallengeSucceeded}
-							> Completei</button>
-						</footer>
-						</div>
-					) : (
-						<div className={styles.challengeNotActive}>
-                <strong>
 
-                    Finalize um ciclo para receber um desafio
-                </strong>
-                <p>
-                    <img src="icons/level-up.svg" alt="Level Up"/>
-                    Avance de level completando desafios
-                </p>
+                            > Falhei</button>
+                            <button
+                                type="button"
+                                className={styles.challengeSucceededButton}
+                                onClick={handleChallengeSucceeded}
+                            > Completei</button>
+                        </footer>
+                    </div>
+                ) : (
+                        <div className={styles.challengeNotActive}>
 
-            </div>
-					) 
-                }
+                            <span className={isDarkMode ? styles.darkTitles : ''}>
+                                <strong>
+                                    Finalize um ciclo para receber um desafio
+                                </strong>
+                                <p>
+                                    <img src="icons/level-up.svg" alt="Level Up" />
+                                    Avance de level completando desafios
+                                </p>
+
+                            </span>
+
+                        </div>
+                    )
+            }
         </div>
     );
 }
