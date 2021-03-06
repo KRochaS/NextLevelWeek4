2c78af62e4ff5898a68d2c892a51726b419d885c
KRochaS <karinerocha.s@hotmail.com>
2021-03-06 19:33:00 -0300

    GET and POST

--- trilha-nodejs/src/server.ts
+++ trilha-nodejs/src/server.ts
@@ -7,7 +7,19 @@ const app = express();
  * GET => Buscar
  * POST => Salvar 
  * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração específica 
  */
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World' });
+});
+
+app.post('/', (request, response) => {
+    // Recebeu dados para salvar
+    return response.json({message: 'Os dados foram salvos com sucesso!'})
+
+});
 
 app.listen(3333, () => {
     console.log('Server is running!');
