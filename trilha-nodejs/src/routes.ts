1cdacfe79f87e67addb0ad487a62c76fb7ac045c
KRochaS <karinerocha.s@hotmail.com>
2021-03-11 19:21:30 -0300

    Surveys routes

--- trilha-nodejs/src/routes.ts
+++ trilha-nodejs/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const useController = new UserController();
+const surveysController = new SurveysController();
 
 router.post("/users", useController.create);
+
+router.post("/surveys", surveysController.create);
 
 export { router };
