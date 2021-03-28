8e273263c83dd6c614e88a5c439f2ad50af4b640
KRochaS <karinerocha.s@hotmail.com>
2021-03-28 00:16:52 -0300

    Survey.test.ts  get

--- trilha-nodejs/src/__tests__/Survey.test.ts
+++ trilha-nodejs/src/__tests__/Survey.test.ts
@@ -18,10 +18,22 @@ describe("Surveys", () => {
 
     it("Should be able to create a new servey", async () => {
         const response = await request(app).post("/surveys").send({
-           title: "Title Example",
-           description: "Description Example",
+            title: "Title Example",
+            description: "Description Example",
         });
 
         expect(response.status).toBe(201);
+        // expect(response.body).toHaveProperty('id');
+    });
+
+    it("Should be able to get all survey", async () => {
+        await request(app).post("/surveys").send({
+            title: "Title Example2",
+            description: "Description Example",
+        });
+
+        const response = await request(app).get("/surveys");
+
+        expect(response.body.length).toBe(2);
     });
 });
