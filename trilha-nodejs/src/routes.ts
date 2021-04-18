e64bac9ac69f78fc9f349192fe198b5c7327082c
KRochaS <karinerocha.s@hotmail.com>
2021-04-18 01:12:02 -0300

    add answers route

--- trilha-nodejs/src/routes.ts
+++ trilha-nodejs/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMail.controller';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
@@ -10,11 +11,15 @@ const surveysController = new SurveysController();
 
 const sendMailController = new SendMailController();
 
+const answerController = new AnswerController();
+
 router.post("/users", useController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
-router.post('/sendMail', sendMailController.execute)
+router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
 
 export { router };
