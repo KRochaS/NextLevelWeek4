e568bc715b7a866242032a207e6132ea73dd5534
KRochaS <karinerocha.s@hotmail.com>
2021-04-18 15:51:38 -0300

    User validations

--- trilha-nodejs/src/controllers/UserController.ts
+++ trilha-nodejs/src/controllers/UserController.ts
@@ -3,9 +3,30 @@ import { getCustomRepository } from 'typeorm';
 
 import { UsersRepository } from '../repositories/UsersRepository';
 
+import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
+
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatório"),
+            email: yup.string().email().required("Email incorreto")
+        });
+
+        // if(!(await schema.isValid(request.body))) {
+        //     return response.status(400).json({error: "Validation Failed!"});
+        // }
+
+        try {
+           await schema.validate(request.body, {abortEarly: false});
+
+        } catch(err) {
+            throw new AppError(err);
+
+            // return response.status(400).json({error: err});
+        }
 
         const usersRepository = getCustomRepository(UsersRepository);
 
@@ -16,9 +37,11 @@ class UserController {
         });
 
         if(userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists!",
-            });
+            throw new AppError("User already exists!");
+
+            // return response.status(400).json({
+            //     error: "User already exists!",
+            // });
         }
 
         const user = usersRepository.create({
